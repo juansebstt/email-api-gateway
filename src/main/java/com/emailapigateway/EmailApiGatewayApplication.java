b7f3b614dfc28fab48f46ab29db8320ab8525c9f
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:02:52 -0500

    first commit

---
+++ src/main/java/com/emailapigateway/EmailApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.emailapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emailapigateway/EmailApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
